2a6a560692edbc9798dc592662ef913f66ca60f6
doubi3 <mark.ekperi@gmail.com>
2020-09-19 21:02:37 +0100

    Navbar complete

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, 
+Switch, 
+Route,  
+} from 'react-router-dom'
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>    
+      <div className="App">
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>   
+          <Route path='/login'>
+            <h1>login</h1>
+          </Route> 
+          <Route path='/'>
+            <Header />
+            <h1>Home</h1>
+          </Route> 
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
